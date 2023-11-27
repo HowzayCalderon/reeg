55a96cc02e47ffff82337716d733f20ebaeab02b
hairyElephant <josecalderon353@gmail.com>
2023-11-26 21:39:15 -0500

    added matcher to middleware, still working on get request

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { headers } from 'next/headers'
 
 const prisma = new PrismaClient()
 
-export async function GET(request: NextRequest){
-    const headerslist = headers()
+export async function GET(request: NextRequest, response: NextResponse){
+    const headerslist = request.headers
     let email = headerslist.get('email') || ''
 
     async function getUser(){
@@ -30,6 +30,7 @@ export async function GET(request: NextRequest){
         await prisma.$disconnect()
         process.exit(1);
     })
+    return response.body
 }
 
 // figure out how to extract username from request and insert it into getUser function
