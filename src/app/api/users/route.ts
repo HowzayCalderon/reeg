0b4b2b56a675711b2a043868e741933ae0f2c572
hairyElephant <josecalderon353@gmail.com>
2023-11-26 21:12:12 -0500

    wrote prototype getUser function

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,14 +1,35 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest } from "next/server";
+import { headers } from 'next/headers'
 
 const prisma = new PrismaClient()
 
 export async function GET(request: NextRequest){
-    const requestHeaders = new Headers(request.headers)
+    const headerslist = headers()
+    let email = headerslist.get('email') || ''
 
-    // function getUser(){
-    //     await prisma.user.findUnique()
-    // }
+    async function getUser(){
+        const user: string = await prisma.user.findUnique({
+            where: {
+                'email': email
+            }
+        }).then(() => {
+            if(user){
+                return user;
+            }else{
+                return 'User not found'
+            }
+        })
+        
+    }
+    getUser()
+    .then(async () => {
+        await prisma.$disconnect()
+    }).catch( async (e) => {
+        console.error(e);
+        await prisma.$disconnect()
+        process.exit(1);
+    })
 }
 
 // figure out how to extract username from request and insert it into getUser function
