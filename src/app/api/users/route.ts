edecaabaa563e79a29d9192d8c8960041943f104
hairyElephant <josecalderon353@gmail.com>
2023-11-27 21:36:56 -0500

    get and post functions not working

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,26 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from 'next/headers'
+import Email from "next-auth/providers/email";
 
 const prisma = new PrismaClient()
 
 export async function GET(request: NextRequest, response: NextResponse){
-    const headerslist = request.headers
-    let email = headerslist.get('email') || ''
-
-    const user = await prisma.user.findUnique({
-        where: {
-            'email': email
-        }
-    })
-
-    if(user){
-        return true;
-    }else{
-        return new Response("User not found");
-    }
+   request.json()
         
     
+}
+
+export async function POST(request: NextRequest, response: NextResponse){
+    request.json()
+    .then(async (res) => {
+        await prisma.user.create({
+            data: {
+                email: res.email,
+                name: res.name
+            }
+        })
+    })
+    return new Response("New User Created")
 }
 
 // figure out how to extract username from request and insert it into getUser function
