39db5612990e0b875a7663749f263201d1f1ddec
hairyElephant <josecalderon353@gmail.com>
2023-12-08 16:17:40 -0500

    google signin working

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -13,10 +13,10 @@ export const options: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }),
-        EmailProvider({
-            server: "",
-            from: ""
-        })
+        // EmailProvider({
+        //     server: "",
+        //     from: ""
+        // })
         
     ],  
     
