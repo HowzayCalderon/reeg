58de72780251e3f3d4fd57565c8ca9d1d9af9831
hairyElephant <josecalderon353@gmail.com>
2023-12-24 17:10:08 -0500

    started writing callback functions

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcrypt'
 
 export const prisma = new PrismaClient()
 
@@ -15,7 +16,7 @@ export const options: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }),
         CredentialsProvider({
-            name: "Email",
+            name: "Email & Password",
 
             credentials: {
                 email:{ label: 'Email', type: 'text', placeholder: 'johnDoe@example.com'},
@@ -32,11 +33,24 @@ export const options: NextAuthOptions = {
                     return null
                 }
                 return getUser
-            }
+            },
+            
         })
     ],  
-    
+    callbacks: {
+        async signIn({user, account, profile, email, credentials }) {
+            const isAllowedToSignIn = true
+            if(isAllowedToSignIn){
+                return true
+            }else{
+                return false
+            }
+        },
+
+    }
 }
 
 /* write getUserFromDB function in Services file for credentials authorize function, 
 refer to next-auth documentation if confused */
+
+/*  FINISH WRITING CALL BACK FUNCTIONS, CONTINUE WATCHING CREDENTIALS TUTORIAL*/
