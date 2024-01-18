5df2a69f74b491b677d812096a8c3854dbfb9390
hairyElephant <josecalderon353@gmail.com>
2024-01-17 22:13:40 -0500

    started credentials provider

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -15,7 +15,43 @@ export const options: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }),
-        
+        CredentialsProvider({
+            name: "Username",
+            credentials: {
+                username: {
+                    label: "Username",
+                    type: "text",
+                    placeholder: "johnDoe"
+                },
+                password: {
+                    label: "Password",
+                    type: "Password",
+                    placeholder: "Enter Password"
+                }
+            },
+            async authorize(credentials){
+                try{
+                    const foundUser = await prisma.user.findUnique({
+                        where: {
+                            email: credentials?.username
+                        }
+                    })
+                    let match = null
+                    if(foundUser){
+                        console.log("User Exists")
+                        match = await bcrypt.compare(credentials?.password as string, foundUser?.password)
+                    };
+                    if(match){
+                        console.log("Good Pass")
+                    }
+    
+                    
+                } catch(error){
+                    console.log(error)
+                }
+                return null
+            }
+        })
     ],
     callbacks: {
         async jwt({token}){
@@ -28,4 +64,4 @@ export const options: NextAuthOptions = {
 }
 
 
-/*  FINISH WRITING CALL BACK FUNCTIONS, CONTINUE WATCHING CREDENTIALS TUTORIAL*/
+// STILL NEED TO CREATE EMAIL VERIFICATION, CUSTOM SIGN IN FORM AND CREATE USER FORM, NEED TO TEST CREDENTIALS PROVIDER SIGN IN
