f0470dd2b071afa6eb22bd5f5e28e2d9035022ea
hairyElephant <josecalderon353@gmail.com>
2024-01-22 23:36:39 -0500

    created signIn and signUp page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "@/components/UserForm/Form";
+
+const SignUp = () => {
+    return (
+        <div>
+            <Form/>
+        </div>
+    )
+}
+
+export default SignUp
