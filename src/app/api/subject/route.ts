96a422cc6e8d8cbaddac818ead0727d2ee0370c8
hairyElephant <josecalderon353@gmail.com>
2024-03-07 21:00:58 -0500

    adds delete to subject route

--- src/app/api/subject/route.ts
+++ src/app/api/subject/route.ts
@@ -22,11 +22,8 @@ export async function POST(request: NextRequest, response: NextResponse){
 export async function GET(request: NextRequest, response: NextResponse){
     try{
         let resMessage: any;
-        const getSubject = await prisma.subject.findFirst({
-            where: {
-                name: 'algebra'
-            }
-        }).then((subject) => {
+        const getSubject = await prisma.subject.findMany()
+        .then((subject) => {
             resMessage = JSON.stringify(subject)
         })
 
@@ -36,5 +33,23 @@ export async function GET(request: NextRequest, response: NextResponse){
     }
 }
 
+export async function DELETE(request: NextRequest, response: NextResponse){
+    try{
+        let resMessage: any = "Subject Deleted"
+        const data = await request.json()
+        const deleteSubject = await prisma.subject.delete({
+            where: {
+                name: data.name
+            }
+        })
+
+        return new Response(resMessage)
+    } catch(err: any){
+        console.error(err)
+        return new Response("Fail")
+    }
+}
+
+
 /* REMEMBER TO CHANGE GETSUBJECT FILTER TO SEARCH FOR THE 
-REQUESTED SUBJECT AND NOT FOR ALGEBRA SPECIFICALLY */
+REQUESTED SUBJECT AND NOT FOR ALGEBRA All of them */
