ee810b2113989dcb8cdc0a397d04203ff2d44c4c
hairyElephant <josecalderon353@gmail.com>
2024-03-07 20:39:47 -0500

    adds protection to dashboard/quiz

--- src/app/dashboard/quiz/page.tsx
+++ src/app/dashboard/quiz/page.tsx
@@ -1,13 +1,24 @@
+'use client'
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
 
+const page = () => {
+    const { data: session } = useSession({
+        required: true,
+        onUnauthenticated(){
+            redirect("/signin")
+        },
+    });
+    
 }
 
 export default page
 
 
-/* MAKE THIS A PROTECTED PAGE, MAKE QUESTIONS APPEAR, 
-WRITE CODE TO ASK USERS TO CHOOSE THEIR ROLE (STUDENT OR TEACHER), 
+/* MAKE QUESTIONS APPEAR, WRITE CODE TO ASK USERS TO CHOOSE 
+THEIR ROLE (STUDENT OR TEACHER), 
 DASHBOARD NEEDS TO INCLUDE THE CLASSES A STUDENT IS A PART OF AND GIVE 
 OPTION TO CHOOSE WHAT SUBJECT THEY WANT TO WORK ON BASED ON THE CLASS */
