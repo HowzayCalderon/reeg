4e8b42d71af59d8069522a19461aa5b69ac7eb6a
hairyElephant <josecalderon353@gmail.com>
2024-03-29 16:58:14 -0400

    adds getUsers

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,19 @@
 import { prisma } from "../auth/[...nextauth]/options"
 import { NextRequest, NextResponse } from "next/server";
 
-
+export async function GET(res: NextResponse, req: NextRequest){
+    try{
+        const resOptions = { status: 200, statusText: "Success"}
+        let resMessage: any = "";
+        const getUsers = await prisma.user.findMany(
+        ).then((res) => {
+            resMessage = JSON.stringify(res)
+        })
+        return new Response(resMessage, resOptions)
+    }catch(e:any){
+        return new Response("it failed");
+    }
+}
 
 
 
