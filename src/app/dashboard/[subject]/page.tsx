280755e7f23e3e632830dc14c64fb4a1daf9a07e
hairyElephant <josecalderon353@gmail.com>
2024-04-06 23:02:00 -0400

    adds subject dynamic route

---
+++ src/app/dashboard/[subject]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const dynamicParams = false;
+
+export function generateStaticParams(){
+    return [{id: "algebra"},{id: "algebra II"}, {id: "chemistry"},
+     {id: "earth science"}, {id: "living environment"},
+      {id: "english language arts"}, {id: "global history and geography"}, 
+    {id: "geometry"}, {id: "united states history and government"}, {id: "physics"}]
+}
+
+export default async function Page({ params }: { params: { id: string }}){
+    const { id } = params
+   let res = await fetch('http://localhost:3000/api/questions', {
+    method: "GET"
+   }).then((data) => data.json())
+   console.log(res)
+}
