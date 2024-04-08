0d3f5d9877262fe1d245c55586fd139a618c2798
hairyElephant <josecalderon353@gmail.com>
2024-04-07 21:08:05 -0400

    adds questions method to subject

---
+++ src/app/api/subject/questions/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest } from "next/server";
+import { prisma } from "@/api/auth/[...nextauth]/options";
+
+export async function GET(request: NextRequest){
+    try{
+        const searchParams = request.nextUrl.searchParams;
+        const getSubject = searchParams.get("subject")
+        const resOptions = {status: 200, statusText: "OK"};
+        let resMessage: any = ""
+        const findQuestions = await prisma.subject.findMany({
+            where:{ name: getSubject as string },
+            include:{
+                questions: true
+            }
+            
+        })
+        .then((data) => {
+            resMessage = JSON.stringify(data)
+        })
+        return new Response(resMessage, resOptions)
+    }catch(e: any){
+
+        return new Response("Failed");
+    }
+}
--- src/app/dashboard/[subject]/page.tsx
+++ src/app/dashboard/[subject]/page.tsx
@@ -3,24 +3,19 @@ import React from "react";
 export const dynamicParams = false;
 export const revalidate = 10;
 
-export function generateStaticParams(){
-    return [{id: "algebra"},{id: "algebra II"}, {id: "chemistry"},
-     {id: "earth science"}, {id: "living environment"},
-      {id: "english language arts"}, {id: "global history and geography"}, 
-    {id: "geometry"}, {id: "united states history and government"}, {id: "physics"}]
+export async function generateStaticParams(){
+    const subjects = await fetch('http://localhost:3000/api/subject')
+    .then((res) => res.json())
+    return subjects.map((sub: any) => {
+        subject: sub.name
+    })
 }
 
-export default async function Page({ params }: { params: { id: string }}){
-    const { id } = params
-   let res = await fetch('http://localhost:3000/api/questions', {
-    method: "GET"
-   }).then((data) => data.json())
-   console.log(res)
-
+export default async function Page({ params }: { params: { subject: string }}){
+    const { subject } = params
+    
 }
 
-/* **************************
-    add questions get method route to return questions 
-    for a specific subject
-    **************************
- */ 
+
+
+//  **   FETCH QUESTIONS FROM SUBJECT MODEL INSTEAD OF QUESTION MODEL
