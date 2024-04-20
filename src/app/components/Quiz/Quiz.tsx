040eb1361bcadb05561f0cdc4106d06eeb30fded
hairyElephant <josecalderon353@gmail.com>
2024-04-19 20:10:52 -0400

    started handleclick function

--- src/app/components/Quiz/Quiz.tsx
+++ src/app/components/Quiz/Quiz.tsx
@@ -11,21 +11,45 @@ interface Options{
 }
 
 function Quiz({A, B, C, D, question, qID}: Options){
-    const [quizData, setQuizData] = useState([{
-        questionID: qID,
+
+    const [ansData, setAnsData] = useState([{
+        questionID: "",
         userAnswer: "",
         isCorrect: ""
     }])
-    
+
+    function handleClick(e:any){
+        e.preventDefault()
+        setAnsData((prevState):any => {
+            [
+                ...prevState,
+                {
+                    questionID: qID,
+                    userAnswer: e?.target?.value,
+                    isCorrect: "false"
+                }
+            ]
+
+        })
+
+    }
+    console.log(ansData)
     return (
         <>
             <p className="border-2 border-black text-center py-4">{question} ?</p>
-                <button className="block pl-5">{A}</button>
-                <button className="block pl-5">{B}</button>
-                <button className="block pl-5">{C}</button>
-                <button className="block pl-5">{D}</button>
+                <input type="button" className="block pl-5" name={A} onClick={handleClick} value={A}/>
+                <input type="button" className="block pl-5" name={B} onClick={handleClick} value={B}/>
+                <input type="button" className="block pl-5" name={C} onClick={handleClick} value={C}/>
+                <input type="button" className="block pl-5" name={D} onClick={handleClick} value={D}/>
         </>
     )
 }
 
 export default Quiz
+
+/*
+    SEND DOWN ALL QUESTIONS FROM PARENT THROUGH PROPS, THEN WRITE LOGIC
+    TO INCREMENT QUESTIONS INSIDE THIS COMPONENT. FINISH WRITING LOGIC 
+    TO SEND ANSWERS TO DATABASE. CONSIDER CREATING A POPUP WINDOW THAT 
+    LETS STUDENTS CHOOSE HOW MANY QUESTIONS THEY ANSWER IN SINGLE SESSION
+ */
