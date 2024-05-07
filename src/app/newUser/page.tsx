b55e5bed714cfdf737e6f6cbeb6c406217bb6f6f
hairyElephant <josecalderon353@gmail.com>
2024-05-06 21:15:10 -0400

    adds gradeLevel

--- src/app/newUser/page.tsx
+++ src/app/newUser/page.tsx
@@ -1,15 +1,18 @@
 "use client"
-import React from "react"
+import React, { useRef } from "react"
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
+
 const Page = () => {
     const router = useRouter()
+    const grade = useRef<HTMLInputElement>(null)
     const { data: session } = useSession()
     const [formData, setFormData] = useState({
         role: "",
         username: "",
+        gradLevel: 0,
         email: session?.user.email,
         id: session?.user.id
     })
@@ -26,6 +29,11 @@ const Page = () => {
         }))
         formData.email = session?.user.email
         formData.id = session?.user.id
+        if(value == "Student"){
+            grade.current ? grade.current.className = visible : null
+        }else if(value == "Teacher"){
+            grade.current ? grade.current.className = hidden : null
+        }
     }
 
     const handleSubmit = async (e: any) => {
@@ -49,16 +57,18 @@ const Page = () => {
         
         */
     }
-
-
+    // styling
+    let hidden = 'hidden'
+    let visible = "block"
     return (
         <div>
             <p>new User Page</p>
-            <form onSubmit={handleSubmit} className="border-black border-2 w-1/2 mx-auto grid grid-cols-2 px-2">
+            <form onSubmit={handleSubmit} className="border-black border-2 w-1/2 mx-auto grid grid-cols-2 p-2">
                 <button  id="teacher" onClick={handleChange} name="role" value="Teacher">Teacher</button>
                 <button id="student" name="role" onClick={handleChange}value="Student">Student</button>
                 <input onChange={handleChange} className="col-span-2 content-center"type="text" name="username" id="username" placeholder="Username"/>
-                <button type="submit">Submit</button>
+                <input type="number" ref={grade} onClick={handleChange}  className={hidden} name="gradeLevel" id="gradeLevel" placeholder="Grade" max={12} min={9}/>
+                <button className="block border-2 col-span-2" type="submit">Submit</button>
             </form>
         </div>
     )
