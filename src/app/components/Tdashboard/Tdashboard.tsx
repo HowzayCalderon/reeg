5e18f98ddbadc94ce91147a2f0e8bcb86514d207
hairyElephant <josecalderon353@gmail.com>
2024-07-09 20:56:09 -0400

    teacher fetch call

--- src/app/components/Tdashboard/Tdashboard.tsx
+++ src/app/components/Tdashboard/Tdashboard.tsx
@@ -1,12 +1,20 @@
+'use client'
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 function Tdashboard({role}: any) {
+  function getTeacher(){
+    const res = fetch('http://localhost:3000/api/teachers')
+  }
 
+  useEffect(() => {
+
+  }, [])
 
   return (
     <div className='grid grid-cols-3 gap-0.5 my-1'>
         <section className='border-black border-2 rounded'>
-          <h1>Account</h1>
+          <h1>{`Welcome ${role}`}</h1>
         </section>
         <section className='border-black border-2 rounded h-10 col-span-2'>
           <h1 className='border-b-2 border-black w-1/2'>Classes</h1>
@@ -16,3 +24,9 @@ function Tdashboard({role}: any) {
 }
 
 export default Tdashboard
+
+/* 
+  REARRANGE THE TEACHER ROUTE TO SPLIT GET AND POST INTO TEACHER/TEACHER AND 
+  TEACHER/TEACHERS AND REWRITE TEACHER GET METHOD TO INCLUDE CLASS INFO
+  .. FINISH FETCH CALL FOR TEACHER DASHBOARD INFORMATION
+*/
