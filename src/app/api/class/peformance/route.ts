2149a3a30884a5fcb530f0fde6cf8d59fdb9527e
hairyElephant <josecalderon353@gmail.com>
2024-09-20 19:24:08 -0400

    adds class performance route

--- src/app/api/class/peformance/route.ts
+++ src/app/api/class/peformance/route.ts
@@ -2,23 +2,23 @@ import { prisma } from "@/api/auth/[...nextauth]/options";
 import { NextRequest } from "next/server";
 
 export async function GET(request:NextRequest) {
-    // try {
-    //     const searchParams = request.nextUrl.searchParams
-    //     const getId: any = searchParams.get('id')
-    //     let resMessage:{} ={}
-    //     const performance = await prisma.topicPerformance.findMany({
-    //         where: {
-    //             student: {userId: getId}
-    //         }
-    //     }).then((data) => {
-    //         resMessage = JSON.stringify(data)
-    //     })
-    //     return new Response("Success",resMessage)
+    try {
+        const searchParams = request.nextUrl.searchParams
+        const classId:URLSearchParams|string|null = searchParams.get('classId')
+        let resMessage:{} ={}
+        const performance = await prisma.topicPerformance.findMany({
+            where: {
+                student: {classId: Number(classId)}
+            }
+        }).then((data) => {
+            resMessage = JSON.stringify(data)
+        })
+        return new Response("Success",resMessage)
         
-    // } catch (error:any) {
-    //     const resOptions = {status: 400, statusText: 'failed'}
-    //     console.log(error)
-    //     return new Response("failed", resOptions)
-    // }
-    
+    } catch (error:any) {
+        const resOptions = {status: 400, statusText: 'failed'}
+        console.log(error)
+        return new Response("failed", resOptions)
+    }
+
 }
